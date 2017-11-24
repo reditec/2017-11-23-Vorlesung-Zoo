0736f22d787fb3d79a52670b78b42fa28dc501f9
reditec <reditec@outlook.com>
2017-11-24 23:49:40 +0100

    2017-11-24
    - Added latest stuff
    - Removed unnecessary files

--- src/Mitarbeiter.java
+++ src/Mitarbeiter.java
@@ -1,12 +1,70 @@
 public class Mitarbeiter {
 
-    public String name = "";
+    private String name = "";
+    private int alter = 0;
+    private char geschlecht = 'n';
 
     public Mitarbeiter() {
         System.out.println("Willkommen...");
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setGeschlecht(char geschlecht){
+        if(geschlecht == 'n' || geschlecht == 'm' || geschlecht == 'w'){
+            this.geschlecht = geschlecht;
+        }
+    }
+
+    public char getGeschlecht(){
+        return geschlecht;
+    }
+
+    public void setAlter(int alter){
+        if(alter > 0){
+            this.alter = alter;
+        } else {
+            this.alter = 0;
+        }
+    }
+
+
+
+    public int getAlter(){
+        return alter;
+    }
+
     public void arbeiten() {
-        System.out.println(name + " arbeitet...");
+        if(alter > 16) {
+            if(name.equals("Horst")) {
+                System.out.println(name + " schläft");
+            }
+            else {
+                System.out.println(name + " arbeitet");
+            }
+        }
+        else {
+            System.out.println(name + " darf noch nicht arbeiten!");
+        }
+    }
+
+    public int gibJahreBisZurRente() {
+        return 67 - alter;
+    }
+
+    public void altern(int jahre){
+        System.out.println("Es sind " + jahre + " Jahre vergangen.");
+        alter += jahre;
+        System.out.println(name + " ist jetzt " + alter + " Jahre alt");
+    }
+
+    public String toString(){
+        return "Name: " + name + ", Alter: " + alter;
     }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -2,13 +2,23 @@ public class Zoo {
 
     public Mitarbeiter[] mitarbeiter;
 
-    public Zoo(){
+    public Zoo() {
         mitarbeiter = new Mitarbeiter[100];
+
         mitarbeiter[0] = new Mitarbeiter();
         mitarbeiter[1] = new Mitarbeiter();
-        mitarbeiter[0].name = "Horst";
+        mitarbeiter[0].setName("Horst");
+        mitarbeiter[1].setName("Horst");
+        mitarbeiter[1].setAlter(-100);
+
+        mitarbeiter[1].altern(20);
+
+        System.out.println(mitarbeiter[0]);
+        System.out.println(mitarbeiter[1]);
+
+        System.out.println("Jahre bis zur Rente: " + mitarbeiter[1].gibJahreBisZurRente());
+
+        mitarbeiter[0].arbeiten();
         mitarbeiter[1].arbeiten();
-
-
     }
 }
